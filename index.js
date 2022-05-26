908a1b5cfad23c9d5601742d28a3938e05699d90
Ifeanyi Beniah Onyebueke <ifeanyionyebueke.ben@gmail.com>
2022-05-26 18:40:04 +0200

    done

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const localRecipe = {
+  title: "the local dish",
+  cuisine:"African Dish"
+}
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,9 +20,46 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async() => {
+    await Recipe.create(localRecipe)
+    .then((recipe)=> console.log("recipe created",recipe))
+   
+    .catch((error) =>
+            console.log("An error happened while:", error)
+          );
   })
+  .then(async() => {
+    await Recipe.insertMany(data)
+    .then((insertData)=> console.log("recipe created",insertData))
+   
+    .catch((error) =>
+            console.log("insertData error:", error)
+          );
+  })
+
+  .then(async() => {
+    await Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{duration:'100'},{
+      new: true})
+    .then((updateData)=> console.log("updated",updateData))
+   
+    .catch((error) =>
+            console.log("updata error:", error)
+          );
+  })
+  .then(async() => {
+    await Recipe.deleteOne({title:'Carrot Cake'})
+    .then((deleteItem)=> console.log("delete one",deleteItem))
+   
+    .catch((error) =>
+            console.log("delete error:", error)
+          );
+  })
+
+
+  
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  mongoose.connection.close()
